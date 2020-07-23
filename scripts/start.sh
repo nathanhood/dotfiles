4f675e920b3e9dcd9689e600ced32ef780471d84
Nathan Hood <nathan.hood@getbuilt.com>
2020-07-22 18:33:15 -0700

    Add start script

---
+++ scripts/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+function daily-notes {
+    current_date=`date +%Y-%m-%d`
+    notes_file="$HOME/notes/built/daily/$current_date.md"
+
+    if [ ! -f "$notes_file" ]; then
+        touch "$notes_file"
+        echo -e "# $current_date Daily Notes\n\n## Retrospective\n\n\n## General Notes" > "$notes_file"
+    fi
+}
+
+function start-day {
+    # Pre-cache sudo credentials for 15min
+    # sudo -v
+
+    source $HOME/.bash_profile
+
+    daily-notes
+
+    if [ "$1" = "fresh" ]; then
+        for APP in "Google Chrome" Slack "Caret Beta" Todoist Postman; do
+            open -a "$APP"
+        done
+    fi   
+
+    cd $HOME/BuiltSource/
+
+    cd ./frontend-auth/ && git checkout -- package* && cd ../cla-miniapp/ && git checkout -- package* && cd ../built-dashboards/ && git checkout -- package* && cd ../cla-local-dev/
+
+    bash $HOME/pull-repos.sh
+}
+
+start-day $1
