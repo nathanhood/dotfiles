ebf8ba36e41586b1c1ceb1ccf19efb24d78b8fc6
Nathan Hood <nathanhood@me.com>
2021-10-19 07:49:33 -0700

    Update start script for recharge

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@
 
 function daily-notes {
     current_date=`date +%Y-%m-%d`
-    notes_file="$HOME/notes/built/daily/$current_date.md"
+    notes_file="$HOME/notes/recharge/daily/$current_date.md"
 
     if [ ! -f "$notes_file" ]; then
         touch "$notes_file"
@@ -22,18 +22,10 @@ function start-day {
     fi
 
     if [ "$1" = "fresh" ]; then
-        for APP in "Google Chrome" Slack "Caret Beta" Todoist Postman; do
+        for APP in "Google Chrome" Slack "Caret Beta" Todoist; do
             open -a "$APP"
         done
     fi   
-
-    if [ "$1" != "simple" ]; then
-        cd $HOME/BuiltSource/
-
-        cd ./frontend-auth/ && git checkout -- package* && cd ../cla-miniapp/ && git checkout -- package* && cd ../built-dashboards/ && git checkout -- package* && cd ../cla-local-dev/
-
-        bash $HOME/scripts/pull-repos.sh
-    fi
 }
 
 start-day $1
