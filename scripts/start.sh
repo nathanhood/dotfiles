b3cce008faf1847088cef5e4dd933ba3b02150d8
Nathan Hood <nathanhood@me.com>
2022-01-31 08:16:29 -0800

    Update start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 
-function daily-notes {
-    current_date=`date +%Y-%m-%d`
-    notes_file="$HOME/notes/recharge/daily/$current_date.md"
-
-    if [ ! -f "$notes_file" ]; then
-        touch "$notes_file"
-        echo -e "# $current_date Daily Notes\n\n## Retrospective\n\n\n## Goals for Today\n\n\n## Stand Up\nWhat did I do?\n\nWhat will I do?\n\nAny blockers?\n\nParking lot\n\nNotes\n\n\n## General Notes\n" > "$notes_file"
-    fi
-}
-
 function start-day {
     # Pre-cache sudo credentials for 15min
     # sudo -v
@@ -18,14 +8,13 @@ function start-day {
     if [ ! -d "$HOME/notes" ]; then
         echo "Notes directory does not exist. Moving on..."
     else
-        daily-notes
+        echo "Need to make note command accessible from start.sh"
+        # python $HOME/notes/scripts/note/main.py daily
     fi
 
-    if [ "$1" = "fresh" ]; then
-        for APP in "Google Chrome" Slack "Caret Beta" Todoist; do
-            open -a "$APP"
-        done
-    fi   
+    for APP in "Google Chrome" Slack "Caret Beta" Postman; do
+        open -a "$APP"
+    done
 }
 
 start-day $1
